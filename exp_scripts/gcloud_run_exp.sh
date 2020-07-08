14e876e3ef4ab862628d3fa3d375dc7d14557df3
MyUbuntu <yz2297@cornell.edu>
2020-07-07 18:47:40 -0700

    tune deploy

--- exp_scripts/gcloud_run_exp.sh
+++ exp_scripts/gcloud_run_exp.sh
@@ -4,5 +4,5 @@ python3 gcloud.py --username yz2297 \
 	--deploy-config social_swarm.json \
 	--stack-name sinan-socialnet \
 	--min-users 4 --max-users 48 --users-step 2 \
-	--exp-time 1200 --measure-interval 1 --slave-port 40011 \
+	--exp-time 1500 --measure-interval 1 --slave-port 40011 \
 	--mab-config social_mab.json
