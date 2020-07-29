89802c2c69204f151c23c1e63ec51b346229bb16
MyUbuntu <yz2297@cornell.edu>
2020-07-28 21:06:34 -0700

    hotel

---
+++ exp_scripts/run_deploy_on_gce_pred_cpu_diurnal.sh
@@ -0,0 +1,8 @@
+cd ../
+python3 master_deploy_diurnal_social.py --user-name yz2297 \
+	--stack-name sinan-socialnet \
+	--min-users 4 --max-users 36 --users-step 4 \
+	--exp-time 120 --measure-interval 1 --slave-port 40011 \
+	--deploy-config social_swarm.json \
+	--gpu-config pred_cpu.json --gpu-port 40010 \
+	--mab-config social_mab.json
