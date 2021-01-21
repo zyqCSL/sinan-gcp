aaf5afaf01d3ba5aa1c37895ffaacaa0a9253476
yanqi <yz2297@cornell.edu>
2021-01-21 11:26:23 +0800

    change account to mingyulianggce

--- locust/scripts/test_social_rps_10.sh
+++ locust/scripts/test_social_rps_10.sh
@@ -1,4 +1,4 @@
-docker run --network host -v $PWD/src:/mnt/locust -v $PWD/base64_images:/mnt/social_images -v $HOME/sinan_locust_log:/mnt/locust_log yz2297/locust_openwhisk \
+docker run --network host -v $PWD/src:/mnt/locust -v $PWD/base64_images:/mnt/social_images -v $HOME/sinan_locust_log:/mnt/locust_log sailresearch/locust_openwhisk \
 	-f /mnt/locust/socialml_rps_10.py \
 	--csv=/mnt/locust_log/social --headless -t $1 \
 	--host http://127.0.0.1:8080 --users $2 \
